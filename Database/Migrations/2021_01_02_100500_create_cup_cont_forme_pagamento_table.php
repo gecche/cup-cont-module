432800e90fc433bdb152abe4ced6c761e9639c43
Giacomo Terreni <giacomoterreni@gmail.com>
2021-01-02 17:18:51 +0100

    publish e immediato pagamento

--- Database/Migrations/2021_01_02_100500_create_cup_cont_forme_pagamento_table.php
+++ Database/Migrations/2021_01_02_100500_create_cup_cont_forme_pagamento_table.php
@@ -19,6 +19,7 @@ class CreateCupContFormePagamentoTable extends Migration {
             			$table->increments('id');
 			$table->string('codice',8)->unique();
 			$table->string('nome');
+            $table->boolean('immediato')->default(1);
 			$table->string('modalita_fatt_el',4)->nullable();
 			$table->text('app_info')->nullable();
 			$table->nullableTimestamps();
--- Providers/CupContServiceProvider.php
+++ Providers/CupContServiceProvider.php
@@ -19,6 +19,7 @@ class CupContServiceProvider extends ServiceProvider
         $this->registerViews();
         $this->registerFactories();
         $this->loadMigrationsFrom(module_path('CupCont', 'Database/Migrations'));
+        $this->cupparisPublish();
     }
 
     /**
@@ -92,6 +93,30 @@ class CupContServiceProvider extends ServiceProvider
         if (! app()->environment('production') && $this->app->runningInConsole()) {
             app(Factory::class)->load(module_path('CupCont', 'Database/factories'));
         }
+    }
+
+
+    public function cupparisPublish()
+    {
+        $this->publishes([
+            __DIR__ . '/../Config/foorms' => config_path('foorms'),
+        ], 'config');
+
+        $this->publishes([
+//            __DIR__ . '/../app/Console/Commands' => app_path('Console/Commands'),
+            __DIR__ . '/../app/Models' => app_path('Models'),
+            __DIR__ . '/../app/Models/Relations' => app_path('Models/Relations'),
+            __DIR__ . '/../app/Policies' => app_path('Policies'),
+            __DIR__ . '/../app/Foorm' => app_path('Foorm'),
+        ], 'models');
+
+        $this->publishes([
+            __DIR__ . '/../public/admin/ModelConfs' => public_path('admin/ModelConfs'),
+            __DIR__ . '/../public/admin/pages' => public_path('admin/pages'),
+            __DIR__ . '/../public/admin/components/js' => public_path('admin/components/js'),
+            __DIR__ . '/../public/admin/components/templates' => public_path('admin/components/templates'),
+        ], 'public');
+
     }
 
     /**
